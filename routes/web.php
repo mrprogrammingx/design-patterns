8e9f08adcf2df3adfcbb8b8b5ec136bfde046c1d
amir <amirjoun2@gmail.com>
2025-03-10 18:40:56 +0400

    Modified Permissions on local

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\Factory\PaymentController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/factory', [PaymentController::class, 'pay']);
